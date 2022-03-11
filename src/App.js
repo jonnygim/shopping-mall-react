fb3b1ca46442b305bf20716e40e1e93a336f3bbd
jjhyunjung <jihyunj22@naver.com>
2022-03-11 12:38:12 +0900

    [Add] banner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import Header from './components/Layout/Header'
 import Main from './components/Layout/Main'
 import Products from './components/Products/Products'
+import './App.css'
+
 
 const App = () => {
 
@@ -10,11 +12,22 @@ const App = () => {
   return (
     <>
       <Header />
+      <Banner />
       <Main>
         <Products />
       </Main>
     </>
   )
 }
+
+function Banner() {
+  return(
+    <>
+    <div className="banner">
+    </div>
+    </>
+  )
+}
+
 
 export default App;
