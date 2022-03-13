8c0fe8b26cf43b80c278e26f2166d8368c4d5e6e
jonnygim <jonnygim.dev@gmail.com>
2022-03-14 05:11:22 +0900

    [Update] Product

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -8,56 +8,23 @@ import { dbService, storageService } from '../../firebaseInstance';
 
 const ProductsList = () => {
   const [products, setProducts] = useState([]);
-  
- 
-  const storage = getStorage();
-  const listRef = ref(storage, 'images/uid');
-  const gsReference = ref(storage, 'gs://shopping-mall-react.appspot.com/images/lamp.jpg');
-  // const httpsReference = ref(storage, 'https://firebasestorage.googleapis.com/b/bucket/o/images%20 lamp.jpg');  
-
-
-  
-
-
-  // getDownloadURL(gsReference)
-  // .then((url) => {
-  //   const xhr = new XMLHttpRequest();
-  //   xhr.responseType = 'blob';
-    
-  //   xhr.onload = (event) => {
-  //     const blob = xhr.response;
-  //   };
-  //   xhr.open('GET', url);
-  //   xhr.send();
-
-  //   // Or inserted into an <img> element
-  //   const img = document.getElementById('myimg');
-  //   img.setAttribute('src', url);
-  // })
-  // .catch((error) => {
-  //   // Handle any errors
-  // });
-  
 
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('https://shopping-mall-react-default-rtdb.firebaseio.com/products.json');
       console.log(response.ok);
-      
-
-     
-      
-
+    
       const responseData = await response.json();
-
       const productsData = [];
       for (const key in responseData) {
+        
         productsData.push({
           id: key,
           name: responseData[key].name,
           price: responseData[key].price,
           charge: responseData[key].deliveryCharge === 0 ? "free" : responseData[key].deliveryCharge,
           company: responseData[key].company,
+          image: key,
         });
 
       }
@@ -75,9 +42,11 @@ const ProductsList = () => {
         price={product.price}
         charge={product.charge}
         company={product.company}
+        image={product.image}
       />
     )
     console.log(productsList);
+    console.log();
   
   return (
     <section className={classes.products}>
